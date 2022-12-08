f68fb78b38eba9977ad66dc154b3e37827b66b01
Maisam Al-Nuaimi <malnu061@uottawa.ca>
2022-12-07 23:01:59 -0500

    fixed test

--- app/src/androidTest/java/alirezamontazer/createbest/productcatalog_v3/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/alirezamontazer/createbest/productcatalog_v3/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("alirezamontazer.createbest.productcatalog_v3-1", appContext.getPackageName());
+        assertEquals("alirezamontazer.createbest.productcatalog_v3", appContext.getPackageName());
     }
 }
